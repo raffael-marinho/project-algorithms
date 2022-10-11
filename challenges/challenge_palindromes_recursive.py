62fbb98a967847bde6e365fe891d719e61a8b846
raffael-marinho <silvamarinho10@gmail.com>
2022-10-11 17:21:12 -0300

    req 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,14 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
+    if word == "":
+        return False
+    if len(word) == 1:
+        return True
+    if word[low_index] == word[high_index]:
+        chars_are_neighbors = high_index == low_index + 1
+        length_is_odd_and_last_character = high_index - 1 == low_index + 1
+        if chars_are_neighbors or length_is_odd_and_last_character:
+            return True
+        return is_palindrome_recursive(word, (low_index + 1), (
+            high_index - 1))
+    else:
+        return False
