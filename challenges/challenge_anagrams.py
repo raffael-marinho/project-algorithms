2ef3e1a6f516bf550bfd656eec681776519973f8
raffael-marinho <silvamarinho10@gmail.com>
2022-10-11 17:25:03 -0300

    req 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,10 @@
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
+    if (len(first_string) != len(second_string)):
+        return False
+    list_first_string = list(first_string.upper())
+    list_second_string = list(second_string.upper())
+    for letter in list_first_string:
+        qnt_letter = list_first_string.count(letter)
+        if (qnt_letter != list_second_string.count(letter)):
+            return False
+    return True
